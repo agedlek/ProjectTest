61e1bc9bf42032ecc2033adbf3d88383ef3950bf
Anna <a.gedlek12@gmail.com>
2025-12-09 18:27:57 +0100

    Add DbContext

---
+++ ProjectTest/Data/ProjectTestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectTest.Model;
+
+namespace ProjectTest.Data
+{
+    public class ProjectTestDbContext: DbContext
+    {
+        public ProjectTestDbContext(DbContextOptions<ProjectTestDbContext> options):base(options)
+        {
+
+
+        }
+
+        public DbSet<Product> Product{ get; set; }
+
+    }
+}
